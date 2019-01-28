f406e71bc3d54482c6237c519915504abc3669f3
jlsoriano86 <josesorna86@gmail.com>
2019-01-28 18:29:51 +0100

    Inicio de la creación de la activity personas.

--- app/src/main/java/com/example/tema6/personas.java
+++ app/src/main/java/com/example/tema6/personas.java
@@ -3,7 +3,7 @@ package com.example.tema6;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Personas extends AppCompatActivity {
+public class personas extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
