b72870d492b1aa8607a10f18e54f51313f026108
jlsoriano86 <josesorna86@gmail.com>
2019-01-28 18:30:59 +0100

    Inicio de la creación de la activity bloc.

--- app/src/main/java/com/example/tema6/MainActivity.java
+++ app/src/main/java/com/example/tema6/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.tema6;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView personas;
+    ImageView bloc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        personas = (ImageView) findViewById(R.id.imagenPersonas);
+        personas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent personas = new Intent(MainActivity.this, com.example.tema6.personas.class);
+                startActivity(personas);
+            }
+        });
+
+        bloc = (ImageView) findViewById(R.id.imagenBloc);
+        bloc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bloc = new Intent(MainActivity.this, com.example.tema6.bloc.class);
+                startActivity(bloc);
+            }
+        });
+
     }
 }
