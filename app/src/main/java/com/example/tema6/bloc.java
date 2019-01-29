6d782d5ec86939b30b6c4cb28bda2139f79b2bf5
jlsoriano86 <josesorna86@gmail.com>
2019-01-29 20:47:04 +0100

    Funciones asignadas a la pantalla del bloc de notas.

--- app/src/main/java/com/example/tema6/bloc.java
+++ app/src/main/java/com/example/tema6/bloc.java
@@ -1,13 +1,45 @@
 package com.example.tema6;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuBuilder;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 public class bloc extends AppCompatActivity {
+    SharedPreferences preferences;
+    EditText txtBloc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bloc);
+        preferences = getSharedPreferences("preferenciasBloc", MODE_PRIVATE);
+        txtBloc = findViewById(R.id.txtBloc);
+        txtBloc.setText(preferences.getString("bloc", ""));
     }
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("bloc", txtBloc.getText().toString());
+        editor.commit();
+        return false;
+    }
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_guardar_bloc, menu);
+        MenuBuilder m = (MenuBuilder) menu;
+        m.setOptionalIconsVisible(true);
+        return true;
+    }
+
+
+
 }
